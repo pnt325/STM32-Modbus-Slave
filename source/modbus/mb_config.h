e4037b42c275d99226cbf43174988eaa51090236
pnt325 <phat.nt@hotmail.com>
2022-06-11 23:43:19 +0700

    Remove macro

--- source/modbus/mb_config.h
+++ source/modbus/mb_config.h
@@ -9,7 +9,6 @@
 #define MB_CONFIG_H_
 
 /* Number of MODBUS slave instance ============================*/
-#define MB_NUM_OF_INSTANCE		1
 #define MB_PDU_SIZE				256
 
 /* STM32 timer configure ======================================*/
@@ -21,26 +20,5 @@
 
 /* Debug configure ============================================*/
 #define MB_DEBUG
-
-//#ifdef MB_DEBUG
-//#define MB_ASSERT
-//#define MB_LOG
-//#endif
-//
-//#ifdef MB_ASSERT
-//extern void __mb_failure(void);
-//#define mb_assert(_v)			(_v)?(void)0:__mb_failure()
-//#else
-//#define mb_assert(_v)
-//#endif
-
-//#ifdef MB_LOG
-//#include "SEGGER_RTT.h"
-//#define mb_log_init()		SEGGER_RTT_ConfigUpBuffer(0, NULL, NULL, 0, SEGGER_RTT_MODE_NO_BLOCK_SKIP)
-//#define mb_log(_c, ...)		SEGGER_RTT_printf(0, _c, ##__VA_ARGS__);
-//#else
-//#define mb_log_init()
-//#define mb_log()
-//#endif
 
 #endif /* MB_CONFIG_H_ */
