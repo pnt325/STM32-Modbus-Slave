0e16775433dabb3d5d62e5be2a906fc9ac54a307
pnt325 <phat.nt@hotmail.com>
2022-06-13 22:51:16 +0700

    Update: edit, remote macro

--- source/modbus/mb_config.h
+++ source/modbus/mb_config.h
@@ -9,14 +9,7 @@
 #define MB_CONFIG_H_
 
 /* Number of MODBUS slave instance ============================*/
-#define MB_PDU_SIZE				256
-
-/* STM32 timer configure ======================================*/
-#define MB_TIMER_32_BIT			0xFFFFFFFF
-#define MB_TIMER_16_BIT			0xFFFF
-
-#define MB_TIMER_RESOLUTION		MB_TIMER_16_BIT				// TODO Configure it
-#define MB_TIMER_CLOCK_SOURCE	100	// Mhz					// TODO Configure it
+#define MB_PDU_SIZE	256
 
 /* Debug configure ============================================*/
 #define MB_DEBUG
